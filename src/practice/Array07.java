203c728a4e1f129a1d13f90878d97b77b16ddb1f
(jmkim) <skawjdals96@gmail.com>
2022-08-18 23:58:25 +0900

    commit

---
+++ src/practice/Array07.java
@@ -0,0 +1,38 @@
+package practice;
+import java.util.*;
+
+public class Array07 {
+	public int solution(int n, int arr[]) {
+		int answer = 0;
+		int cnt = 0;
+		
+		for(int i = 0; i<n; i++) {
+			if(arr[i]==1) {
+				cnt++;
+				answer+=cnt;
+			}
+			else cnt = 0;
+		}
+		
+		return answer;
+		
+	}
+	
+	public static void main(String args[]) {
+		
+		Array07 T = new Array07();
+		Scanner kb = new Scanner(System.in);
+		
+		int n = kb.nextInt();
+		int arr[] = new int[n];
+		
+		for(int i = 0; i<n; i++ ) {
+			arr[i] = kb.nextInt();
+		}
+		
+		System.out.print(T.solution(n,arr));
+		
+		
+	}
+
+}
