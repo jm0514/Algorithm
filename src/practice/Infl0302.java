ab2a658407f303cc96b4cad4a55188c45e063a4a
(jmkim) <skawjdals96@gmail.com>
2022-08-31 23:32:55 +0900

    8/31 Algorithm

--- src/practice/Infl0302.java
+++ src/practice/Infl0302.java
@@ -2,23 +2,29 @@ package practice;
 import java.util.*;
 
 public class Infl0302 {
-	public ArrayList<Integer> solution (int n, int m, int arr1[], int arr2[]){
+	public ArrayList<Integer> solution (int n, int m, int[] arr1, int[] arr2){
 		ArrayList<Integer> answer = new ArrayList<>();
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
 		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<m; j++) {
-			if(arr1[n] == arr2[m]) {
-				answer.add(arr2[m]);
-			}
-			}
+		int p1=0;
+		int p2=0;
+		
+		while(p1<n && p2<m) {
+			if(arr1[p1] == arr2[p2]) {
+				answer.add(arr1[p1++]);
+				p2++;
+			}else if(arr1[p1]<arr2[p2]) {
+				p1++;
+			}else p2++;
 		}
 		return answer;
 	}
+		
 	
 	public static void main(String[] args) {
 		Infl0302 T = new Infl0302();
 		Scanner kb = new Scanner(System.in);
-		
 		
 		int n = kb.nextInt();
 		int arr1[] = new int[n];
