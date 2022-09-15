5f1e6669b189b758cb4362b562c48a19ba5b4382
(jmkim) <skawjdals96@gmail.com>
2022-09-15 18:33:46 +0900

    Stack

---
+++ Infl501.java
@@ -0,0 +1,34 @@
+package practice;
+import java.util.*;
+
+public class Infl501 {
+	public String solution(String str) {
+		String answer = "YES";
+		Stack<Character> stack = new Stack<>();
+		for(char x : str.toCharArray()) {
+			if(x=='(') {
+				stack.push(x);
+			}else {
+				if(stack.isEmpty()) {
+					return "NO";
+				}
+				stack.pop();
+			}
+		}
+		if(!stack.isEmpty()) {
+			return  "NO";
+		}
+		
+		return answer;
+	}
+		
+	public static void main(String[] args) {
+		Infl501 T = new Infl501();
+		Scanner kb = new Scanner(System.in);
+		
+		String str = kb.next();
+		
+		System.out.print(T.solution(str));
+	}
+	
+}
