dfe1f3d8e4fe64d169909611eb1a31bbe085ffc2
(jmkim) <skawjdals96@gmail.com>
2022-09-21 22:00:58 +0900

    Stack(Profix)

--- src/algorithm/Infl504.java
+++ src/algorithm/Infl504.java
@@ -7,9 +7,9 @@ public class Infl504 {
 		Stack<Integer> stack = new Stack<>();
 		
 		for(char x : str.toCharArray()) {
-			if(Character.isDigit(x)) {
+			if(Character.isDigit(x)) { //숫자판별
 				stack.push(x-48);
-			}else {
+			}else { //후위 연산이기 때문에 스택에 2개 이상의 숫자가 존재한다.
 				int rt = stack.pop();
 				int lt = stack.pop();
 				if(x=='+') {
@@ -24,7 +24,7 @@ public class Infl504 {
 				
 			}
 		}
-		answer = stack.get(0);
+		answer = stack.get(0); //stack의 0번째 값을 얻는다.
 		
 		return answer;
 	}
