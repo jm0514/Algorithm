139696c8b81339e9debf5e5e06ce361d69dcb17f
(jmkim) <skawjdals96@gmail.com>
2022-09-23 23:59:55 +0900

    Stack

---
+++ src/algorithm/Infl505.java
@@ -0,0 +1,34 @@
+package algorithm;
+import java.util.*;
+
+public class Infl505 {
+	public int solution(String str) {
+		int answer = 0;
+		Stack<Character> stack = new Stack<>();
+		
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i) == '(') {
+				stack.push('(');
+			}else {
+				stack.pop();
+				if(str.charAt(i-1) == '(') {
+					answer += stack.size();
+				}else {
+					answer++;
+				}
+			}
+		}
+		
+		
+		return answer;
+	}
+		
+	public static void main(String[] args) {
+		Infl505 T = new Infl505();
+		Scanner kb = new Scanner(System.in);
+		
+		String str = kb.next();
+		
+		System.out.print(T.solution(str));
+	}
+}
