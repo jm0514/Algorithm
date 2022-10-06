1f8bf11e510bcf888ee0a66c27db1e6118f53b52
(jmkim) <skawjdals96@gmail.com>
2022-10-06 23:30:51 +0900

    LRU

--- src/algorithm/Infl604.java
+++ src/algorithm/Infl604.java
@@ -3,30 +3,30 @@ import java.util.*;
 
 public class Infl604{
 	
-	public int[] solution(int size, int n, int arr[]) {
+	public int[] solution(int size, int n, int input[]) {
 		
 		int cache[] = new int[size];
 		
-		for(int x:arr) {
+		for(int x : input) {
 			int pos = -1;
-			for(int i=0; i<size; i++ ) {
+			for(int i = 0; i< size; i++) {
 				if(x==cache[i]) {
-					pos=i;
-				}
-				if(pos==-1) {
-					for(int i = size-1; i>=1; i--) {
-						cache[i]=cache[i-1];
-					}
-					cache[0]=x;
-				}else {
-					for(int i = pos; i>=1; i--) {
-						cache[i]=cache[i-1];
-					}
-					cache[0] = x;
+					pos = i;
 				}
 			}
+			
+			if(pos == -1) {
+				for(int i = size -1; i >= 1; i--) {
+				cache[i] = cache[i-1];
+				}
+			}
+			else{
+				for(int i = pos -1; i >= 1; i--) {
+				cache[i] = cache[i-1];
+				}
+			}
+			cache[0] = x;
 		}
-		
 		return cache;
 		
 	}
@@ -39,13 +39,13 @@ public class Infl604{
 	int s = kb.nextInt();
 	int n = kb.nextInt();
 	
-	int arr[] = new int[n];	
+	int input[] = new int[n];	
 	
 	for(int i =0; i<n; i++) {
-		arr[i] = kb.nextInt();
+		input[i] = kb.nextInt();
 	}
 	
-	for(int x : T.solution(s, n , arr)) System.out.print(x+" ");
+	for(int x : T.solution(s, n , input)) System.out.print(x+" ");
 	}
 	
 }
